9e041362b3f0448ab07d07f5d6d67348d70316a0
Deepdasolution <diplove32928@gmail.com>
2019-03-14 19:56:37 +0545

    First commit

---
+++ backend/src/main/java/movie_recommender/movie_recommender_backend/App.java
@@ -0,0 +1,13 @@
+package movie_recommender.movie_recommender_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
